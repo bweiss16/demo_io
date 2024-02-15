184efc764eab9bc716204620d00b4734d51445f5
ben <bweiss@whoi.edu>
2024-02-15 13:01:19 -0500

    changed main.cpp and slowed down

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BLINK_DELAY 250
+int led = 13;
+int n = 0;
 
+// the setup routine runs once when you press reset:
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  }
 
+// the loop routine runs over and over again forever:
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(BLINK_DELAY);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(BLINK_DELAY);               // wait for a second
+  Serial.println(sin(2*n*3.14/20));
+  n++;
 }
