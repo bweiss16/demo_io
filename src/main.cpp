aa576c75edca12afec77310f14140a3cd9159d43
ben <bweiss@whoi.edu>
2024-02-16 15:18:24 -0500

    wrote a littl function to concatenate strings and ints

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,37 @@
 /*
- * TimeRTC.pde
- * example code illustrating Time library with Real Time Clock.
- * 
+ * Trying to develop a function that combines an integr anda string in C++
+ * I've also chagned printAndLog to expect char string[] as opposed to const char string[]
+ * since this input IS changing
  */
 
 #include <Arduino.h>
-#include <TimeLib.h>
+using namespace std;
+#include <string>
 
-void printDigits(int digits){
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
+// Initialize counter
+int i = 0;
 
-void digitalClockDisplay() {
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
-}
-
-time_t getTeensy3Time()
-{
-  return Teensy3Clock.get();
-}
-
-/*  code to process time sync messages from the serial port   */
-#define TIME_HEADER  "T"   // Header tag for serial time sync message
-
-unsigned long processSyncMessage() {
-  unsigned long pctime = 0L;
-  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
-
-  if(Serial.find(TIME_HEADER)) {
-     pctime = Serial.parseInt();
-     return pctime;
-     if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
-       pctime = 0L; // return 0 to indicate that the time is not valid
-     }
+// Wrap logging and serial print into a single function
+void printAndLog(const char string[]){
+  Serial.println(string);
+  //logSD(string); //commented out for now because no SD connected
   }
-  return pctime;
+
+// Add an integer to a string
+const char * customAdd(std::string string, int b){
+    std::string concat = string + std::to_string(b);
+    const char * result = concat.c_str(); //convert string to pointer
+    return result;  
 }
 
 void setup()  {
-  // set the Time library to use Teensy 3.0's RTC to keep time
-  setSyncProvider(getTeensy3Time);
-
-  Serial.begin(115200);
-  while (!Serial);  // Wait for Arduino Serial Monitor to open
-  delay(100);
-  if (timeStatus()!= timeSet) {
-    Serial.println("Unable to sync with the RTC");
-  } else {
-    Serial.println("RTC has set the system time to");
-  }
+  Serial.begin(9600);
+  Serial.println("Setup Loop ran");
+  delay(500);
 }
 
 void loop() {
-  if (Serial.available()) {
-    time_t t = processSyncMessage();
-    if (t != 0) {
-      Teensy3Clock.set(t); // set the RTC
-      setTime(t);
-    }
-  }
-  digitalClockDisplay();  
-  delay(1000);
+  printAndLog(customAdd("Integer is ", i));
+  i++;
+  delay(200);
 }
