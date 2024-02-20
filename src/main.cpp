50381926defe685b6da2622aafce53c74d86b513
ben <bweiss@whoi.edu>
2024-02-20 13:32:47 -0500

    create function to read in  configuratoins from an SD card

--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,166 @@
 /*
- * create a function that allows you to break down time strings
- * and access the hr min and sec as integers.
+ * create function to read in all configuratoins from seperate file
+ * note that variables here, at top of file are instantiated but not declared
  */
 
 #include <Arduino.h>
-using namespace std;
-#include <string>
-#include <array>
+#include <SD.h>
+#include <SDConfig.h>
 
-//Pound defines linking alarm number with hour prefix
-#define ALARM_1 "12:00:56"
-#define ALARM_2 (19)
-#define ALARM_3 (20)
-#define ALARM_4 (21)
-#define ALARM_5 (22)
-#define ALARM_6 (23)
-#define ALARM_7 (0)
-#define ALARM_8 (1)
-#define ALARM_9 (2)
-#define ALARM_10 (3)
-#define ALARM_11 (4)
-#define ALARM_12 (5)
+// STATIC CONFIGS FOR SD
+// SDCARD_SS_PIN is defined for the built-in SD on some boards.
+#ifndef SDCARD_SS_PIN
+const uint8_t SDCARD_CS_PIN = SS; //SS Teensy 4.1
+#else  // SDCARD_SS_PIN
+const uint8_t SDCARD_CS_PIN = 10; //CS pin should be 10 w our config of audio shield
+#endif  // SDCARD_SS_PIN
+// Other
+#define SDCARD_MOSI_PIN  7
+#define SDCARD_SCK_PIN   14
 
-#define ALARM_1_FILENAME "18TKP1.WAV"
+//SDConfig Settings
+const char * fileName = "config.cfg";
+int maxLineLength = 30;
+SDConfig cfg;
 
-// Declare sampel number;
-int sampleNumber = 0;
+//instantiate vars that will be filled in by config file
+//Alarm times
+char *ALARM_1;
+char *ALARM_2;
+char *ALARM_3;
+char *ALARM_4;
+char *ALARM_5;
+char *ALARM_6;
+char *ALARM_7;
+char *ALARM_8;
+char *ALARM_9;
+char *ALARM_10;
+char *ALARM_11;
+char *ALARM_12;
+// Wake Time
+int startH;
+int startM;
+int startS;
+// Play Time (first alarm) [18:0:0 for real]
+int playH;
+int playM;
+int playS;
+// Sleep Time
+int stopH;
+int stopM;
+int stopS;
+//other
+char *SAMPLE_LOCATION; 
+int BAUDE_RATE;
 
-// Wrap logging and serial print into a single function
-void printAndLog(const char string[]){
-  Serial.println(string);
-  //logSD(string); //commented out for now because no SD connected
+//config function
+void getSettings(){
+  if(cfg.begin(fileName, maxLineLength)){
+    Serial.println("Config file opened."); 
+    while (cfg.readNextSetting()) {
+      if(cfg.nameIs("ALARM_1")){
+        ALARM_1 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_2")) {
+        ALARM_2 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("ALARM_3")){
+        ALARM_3 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_4")) {
+        ALARM_4 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("ALARM_5")){
+        ALARM_5 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_6")) {
+        ALARM_6 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("ALARM_7")){
+        ALARM_7 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("ALARM_8")){
+        ALARM_8 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_9")) {
+        ALARM_9 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("ALARM_10")){
+        ALARM_10 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_11")) {
+        ALARM_11 = cfg.copyValue();
+      }
+      else if (cfg.nameIs("ALARM_12")) {
+        ALARM_12 = cfg.copyValue();
+      }
+      else if(cfg.nameIs("SAMPLE_LOCATION")){
+        SAMPLE_LOCATION = cfg.copyValue();
+      }      
+      else if(cfg.nameIs("BAUDE_RATE")){
+        BAUDE_RATE = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("startH")){
+        startH = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("startM")){
+        startM = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("startS")){
+        startS = cfg.getIntValue();
+      }                   
+      else if(cfg.nameIs("playH")){
+        playH = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("playM")){
+        playM = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("playS")){
+        playS = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("stopH")){
+        stopH = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("stopM")){
+        stopM = cfg.getIntValue();
+      }
+      else if(cfg.nameIs("stopS")){
+        stopS = cfg.getIntValue();
+      }            
+      else{
+      }
+    }
+    cfg.end();
   }
-
-// Add an integer to a string
-const char * customAdd(std::string string, int b){
-    std::string concat = string + std::to_string(b);
-    const char * result = concat.c_str(); //convert string to pointer
-    return result;  
-}
-
-// construct playback file name from an hour + sample number (e.g. 18TKP1.WAV)
-const char * makeFileName(int hr, int samp){
-    std::string hour = "00";
-    if (hr < 10){
-      hour = "0" + std::to_string(hr);
-    }
-    else {
-      hour = std::to_string(hr);
-    }
-    
-    std::string concat = hour + "TKP" + std::to_string(samp) + ".WAV";
-    const char * result = concat.c_str(); //convert string to pointer
-    return result;  
-}
-
-//Fake playFile function
-void playFile(const char string[]){
-  Serial.print("Now playing ");
-  Serial.println(string);
-}
-
-std::array<int,3> timeConstruct(std::string timeString){
-  std::array<int,3> timeInts;
-  std::string hrString = timeString.substr(0,2);
-  std::string minString = timeString.substr(3,4);
-  std::string secString = timeString.substr(6,7);
-  timeInts[0] = stoi(hrString);
-  timeInts[1] = stoi(minString);
-  timeInts[2] = stoi(secString);
-
-  return timeInts;
-}
-
-// ALARM FUNCTIONS
-// Audio file 1
-void startPlayingAlarm1() {
-
-  std::array<int,3> timeIntegers = timeConstruct(ALARM_1);
-  Serial.println("Alarm Time hour: ");
-  Serial.println(timeIntegers[0]);
-  Serial.println("Alarm Time min: ");
-  Serial.println(timeIntegers[1]);
-  Serial.println("Alarm Time sec: ");
-  Serial.println(timeIntegers[2]);
-
-  printAndLog("Alarm1");
-  to_string(timeIntegers[0]);
-  playFile(ALARM_1_FILENAME);
-
-  /*
-  if (sampleNumber == 1){
-    playFile("18TKP1.WAV");  // filenames are always uppercase 8.3 format
-    }
-  else if (sampleNumber == 2){
-    playFile("18TKP2.WAV");
-    } 
-  else if (sampleNumber == 3){
-    playFile("18TKP3.WAV");
-    }
-  else if (sampleNumber == 4){
-    playFile("18TKP4.WAV");
-    }
-  else {
-    printAndLog("sampleNumber did not register. Default = 1");
-    playFile("18TKP1.WAV");
-  }
-  */
-
-  delay(250);
+  Serial.println("Could not open config file.");
 }
 
 void setup()  {
   Serial.begin(9600);
   delay(500);
+  
+  // Config file Read setup
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SDCARD_CS_PIN)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("SD initialization succeeded.");
+   
+  // get settings from file
+  getSettings();
+  
+  // Print results
+  Serial.println("Loaded configuration values:");
+  Serial.println((String)"ALARM_1 is = " + ALARM_1);
+  Serial.println((String)"ALARM_9 is = " + ALARM_9);
+  Serial.println((String)"SAMPLE_LOCATION is = " + SAMPLE_LOCATION);
+  Serial.println((String)"startH is = " + startH);
+
   Serial.println("Setup Loop complete");
   delay(500);
 }
 
 void loop() {
-  startPlayingAlarm1();
-  delay(5000);
+  //Do nothing
 }
